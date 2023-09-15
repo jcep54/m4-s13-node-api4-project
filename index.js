22553a489ec5e7e3b4b64d428fd55472e819f63f
Jose Cepero <ceperojose54@gmail.com>
2023-09-15 09:04:33 -0400

    wip

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 const express = require('express')
+const cors = require('cors')
+require('dotenv').config()
+
+console.log(process.env.PORT, process.env.NODE_ENV)
 
 const server = express();
 
 server.use(express.json())
+server.use(cors())
 
 server.get('/testing', ( req, res ) =>{
     res.json(`we're up and cooking`)
 })
 
-const port = 9000
-const newPort = process.env.PORT
+const port = process.env.PORT || 9000
 
 server.listen( port, () =>{
     console.log(`sever running on ${port}`)
